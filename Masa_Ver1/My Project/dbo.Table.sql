35295340c06e1508bb7fc5a9c484c0f2d7af801a
wayne.elseth <wayne.elseth@outlook.com>
2016-02-12 10:00:44 -0500

    Added in the sql files stored in the "wrong place" on my local computer.

---
+++ Masa_Ver1/My Project/dbo.Table.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NTEXT NOT NULL, 
+    [Instructor] BIT NULL, 
+    [Tow Pilot] BIT NULL
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Yes/No',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Table',
+    @level2type = N'COLUMN',
+    @level2name = N'Instructor'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Yes/No',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Table',
+    @level2type = N'COLUMN',
+    @level2name = N'Tow Pilot'
